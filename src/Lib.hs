15960d0632acb4d5b0f4a32c3f39eff492effc13
corona6054 <valen.algo@gmail.com>
2022-06-08 14:57:58 -0300

    punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,11 @@
 {-
 Nombre: dubreucq, agustin
-Legajo: 999999-9
+Legajo: 178247-2
 -}
 
 module Lib where
 import Text.Show.Functions
+import Data.Fixed (HasResolution)
 
 ----------------------------------------------
 -- Código base provisto en el enunciado
@@ -32,4 +33,49 @@ data Desafio = Desafio {
 -- Definí tus tipos de datos y funciones aquí
 -- abajo, indicando a qué punto pertenecen
 ----------------------------------------------
+
+data Participante = UnParticipante {
+  nombre :: String,
+  experiencia :: Int,
+  inteligencia :: Int,
+  destreza :: Int,
+  rol :: Rol
+}
+
+data Arma = UnArma {
+  valorCombate :: Int,
+  experienciaMinima :: Int
+}
+
+type Rol = Participante -> Int
+
+indeterminado :: Participante -> Int
+indeterminado participante = destreza participante + inteligencia participante
+
+soporte :: Participante -> Int
+soporte participante = experiencia participante + inteligencia participante*7
+
+primeraLinea :: Arma -> Participante -> Int
+primeraLinea arma participante = (destreza participante + potenciaArma arma participante) * div (experiencia participante) 100
+
+
+potenciaArma :: Arma -> Participante -> Int
+potenciaArma arma participante 
+  |experienciaMinima arma <= experiencia participante = valorCombate arma
+  | otherwise = div (valorCombate arma) 2
+
+participante = UnParticipante "participante" 1000 20 12 indeterminado
+
+poder :: Participante -> Int
+poder participante = experiencia participante * rol participante participante
+
+{-------------------------------punto 2--------------------------------------}
+--elegirNuevoRol :: Participante -> [Rol] -> Participante
+--elegirNuevoRol participante roles = 
+
+
+
+
+
+
 
